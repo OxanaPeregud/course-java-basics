9dd1c77fbbacc43521b489feb4631cdb8e7c04aa
OxanaPeregud <ox.peregud@gmail.com>
2021-02-14 18:23:00 +0300

    solution: Done 'Enum Day' for theme 'conditional-statements'

---
+++ src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Day.java
@@ -0,0 +1,11 @@
+package com.rakovets.course.javabasics.practice.conditionalstatements;
+
+enum Day {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
